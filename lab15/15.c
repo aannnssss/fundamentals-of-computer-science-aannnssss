2f8a6a7e2ff02148cd8726e6b197c7f901bdb343
aannnssss <114110228+aannnssss@users.noreply.github.com>
2022-12-15 02:07:48 +0300

    Add files via upload

---
+++ lab15/15.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+enum {maxn = 8};
+
+void input(int n, int (*m)[n]);
+void process(int n, int (*m)[n]);
+
+int main() {
+    int m[maxn][maxn];
+    int n;
+    scanf("%d",&n);
+    input(n, (int (*)[n]) m);
+    printf("\n");
+    process(n, (int (*)[n]) m);
+    return 0;
+}
+void input(int n, int (*m)[n]) {    
+    for (int i = 0; i != n; i += 1) {
+        for (int j = 0; j != n; j += 1) 
+            scanf("%d", &m[i][j]); 
+    }
+}
+
+void process (int n, int (*m)[n]) {
+    int i = 0, j = 0, maxelem = -10000000;
+    while (n > i) {
+        while (j < n) {
+            if ((m[i][j]) > maxelem) {
+                maxelem = m[i][j];
+                j += 1; 
+            } else 
+                j += 1;
+        }
+        j = i;
+        m[i][j] = maxelem;
+        for (int j = 0; j < n; j += 1) 
+            printf("%d ", m[i][j]);
+        printf("\n");
+        maxelem = -10000000;
+        i += 1;
+        j = 0;
+    }
+}   
