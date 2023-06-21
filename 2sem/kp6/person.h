8b47980678c89a7a7a8837fc13d8fa758ac7191c
aannnssss <starostina-anuta@mail.ru>
2023-06-21 14:58:48 +0300

    fixing

--- 2sem/kp6/person.h
+++ 2sem/kp6/person.h
@@ -1,5 +1,6 @@
 #ifndef PERSON_H
 #define PERSON_H
+#define LINE_SIZE (40U)
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -8,13 +9,17 @@
 typedef unsigned int uint;
 
 typedef struct {
-    char surname[50];
-    char initials[2];
+    char surname[LINE_SIZE];
+    char initials[LINE_SIZE];
     char gender;
     uint school_number;
     bool medal;
     uint marks;
     bool essay;
 } Person;
+
+void usage(){
+    printf("Usage: program filename\n");
+}
 #endif //PERSON_H
 
